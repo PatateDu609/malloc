1e4b09bf9686b2013ebe4f8853414e71e4d3b2d0
PatateDu609 <gboucett@student.42.fr>
2022-04-24 16:33:18 +0200

    fixing overhead on free

--- src/zone/free_zone.c
+++ src/zone/free_zone.c
@@ -1,8 +1,24 @@
 #include "zones.h"
+#include "utils.h"
+
+static u_int8_t is_last_of_type(t_zone *zone)
+{
+	t_zone *z = g_zones;
+
+	if (zone->type == LARGE)
+		return 0;
+	while (z)
+	{
+		if (zone != z && z->type == zone->type)
+			return 0;
+		z = z->next;
+	}
+	return 1;
+}
 
 void free_zone(t_zone *zone)
 {
-	if (!IS_ZONE_EMPTY(zone))
+	if (!IS_ZONE_EMPTY(zone) || is_last_of_type(zone))
 		return;
 	remove_zone(zone);
 	munmap(zone, zone->size);
--- testing/test2.c
+++ testing/test2.c
@@ -13,8 +13,8 @@ int main(void)
     {
         addr = (char*)malloc(1024);
         addr[0] = 42;
-        free(addr); 
-        i++; 
+        free(addr);
+        i++;
     }
     return (0);
 }
